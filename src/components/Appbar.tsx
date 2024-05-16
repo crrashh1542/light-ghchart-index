231f4fdb77a93c11a1c5173c1c707d0fa008e631
crrashh1542 <kotsuki@crrashh.com>
2024-05-17 01:06:22 +0800

    feat: 添加右上角GitHub图标
    通过 flex 布局实现标题与图标分局 appbar 两侧。
    
    Signed-off-by: crrashh1542 <kotsuki@crrashh.com>

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,6 +1,11 @@
+import githubImg from '../assets/images/github.svg'
 import '../styles/appbar.less'
 
 export default () =>
    <header>
       <a className="title" href="#">GitHub Chart API</a>
+      <div className="grow"></div>
+      <a href="https://github.com/crrashh1542/light-ghchart-index">
+         <img src={ githubImg } alt="本引导页的 GitHub 项目地址" />
+      </a>
    </header>
