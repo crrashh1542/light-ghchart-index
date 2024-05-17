2fa46e061b79b86f22ff905b0f13482677e656fb
crrashh1542 <kotsuki@crrashh.com>
2024-05-18 01:11:20 +0800

    build: 移除chunk分析工具
    Signed-off-by: crrashh1542 <kotsuki@crrashh.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite'
-import { visualizer } from 'rollup-plugin-visualizer'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [
-      react(),
-      visualizer({
-         gzipSize: false,
-         brotliSize: true,
-         emitFile: false,
-         filename: "test.html",
-         open: false
-       })
+      react()
    ],
    resolve: {},
    server: {
